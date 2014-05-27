b397eb8e3f0ac531c6177098c180d0bef53ec639
Kevin Lovato <alprema@alprema.com>
2014-05-27 15:05:10 +0200

    Update version number and fix install package script

--- build/nuget/tools/Zebus/Install.ps1
+++ build/nuget/tools/Zebus/Install.ps1
@@ -1,4 +1,4 @@
 param($installPath, $toolsPath, $package, $project)
 
-$project.Properties.Item("PreBuildEvent").Value = 'xcopy "C:\Dev\dotnet\lib\ZeroMq\libzmq-x64-0.0.0.0.dll" "$(TargetDir)" /Y 
-xcopy "C:\Dev\dotnet\lib\ZeroMq\libzmq-x86-0.0.0.0.dll" "$(TargetDir)" /Y'
+$project.Properties.Item("PreBuildEvent").Value = 'xcopy "'+$toolsPath+'\libzmq-x64-0.0.0.0.dll" "$(TargetDir)" /Y 
+xcopy "'+$toolsPath+'\libzmq-x86-0.0.0.0.dll" "$(TargetDir)" /Y'
