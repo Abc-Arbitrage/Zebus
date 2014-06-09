3efdb2b1de272b7b55dcbf5e4855ea3e6a18f88b
Kevin Lovato <alprema@alprema.com>
2014-06-09 16:29:15 +0200

    Added events / commands to handle incremental directory updates

---
+++ src/Abc.Zebus/Directory/AddPeerSubscriptionsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ProtoBuf;
+
+namespace Abc.Zebus.Directory
+{
+    [ProtoContract, Transient]
+    public class AddPeerSubscriptionsCommand : ICommand
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public readonly PeerId PeerId;
+
+        [ProtoMember(2, IsRequired = true)]
+        public readonly Subscription[] Subscriptions;
+
+        [ProtoMember(3, IsRequired = false)]
+        public readonly DateTime? TimestampUtc;
+
+        public AddPeerSubscriptionsCommand(PeerId peerId, Subscription[] subscriptions, DateTime logicalClock)
+        {
+            PeerId = peerId;
+            Subscriptions = subscriptions;
+            TimestampUtc = logicalClock;
+        }
+
+        public override string ToString()
+        {
+            return PeerId.ToString();
+        }
+    }
+}
---
+++ src/Abc.Zebus/Directory/PeerSubscriptionsRemoved.cs
@@ -0,0 +1,25 @@
+﻿using ProtoBuf;
+
+namespace Abc.Zebus.Directory
+{
+    [ProtoContract, Transient]
+    public sealed class PeerSubscriptionsRemoved: IEvent
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public readonly PeerId PeerId;
+
+        [ProtoMember(2, IsRequired = true)]
+        public readonly Subscription[] Subscriptions;
+
+        public PeerSubscriptionsRemoved(PeerId peerId, Subscription[] subscriptions)
+        {
+            PeerId = peerId;
+            Subscriptions = subscriptions;
+        }
+
+        public override string ToString()
+        {
+            return PeerId.ToString();
+        }
+    }
+}
