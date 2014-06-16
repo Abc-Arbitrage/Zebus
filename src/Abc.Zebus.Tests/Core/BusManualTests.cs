5d916c0f531c318ac02a7793f53e4defa44b7aa4
Olivier Coanet <olivier.coanet@gmail.com>
2014-06-16 18:08:08 +0200

    manual test added

--- src/Abc.Zebus.Tests/Core/BusManualTests.cs
+++ src/Abc.Zebus.Tests/Core/BusManualTests.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Threading;
 using ABC.ServiceBus.Contracts;
 using Abc.Zebus.Core;
+using Abc.Zebus.Dispatch;
+using Abc.Zebus.Testing;
 using Abc.Zebus.Testing.Extensions;
 using Abc.Zebus.Util;
 using NUnit.Framework;
@@ -15,6 +17,109 @@ namespace Abc.Zebus.Tests.Core
     [TestFixture, Ignore("Manual test")]
     public class BusManualTests
     {
+        [Test]
+        public void SendSeveralTimeoutRequestAndCheckForResponseUnicity()
+        {
+            var handler = new AccumulatingTimeoutCommandHandler();
+
+            var bus = CreateBusFactory() 
+                .WithHandlers(typeof(AccumulatingTimeoutCommandHandler))
+                .ConfigureContainer(x => x.ForSingletonOf<AccumulatingTimeoutCommandHandler>().Use(handler))
+                .CreateAndStartBus();
+
+            bus.Subscribe(Subscription.Matching<TimeoutCommand>(x => x.ServiceName == bus.PeerId.ToString()));
+
+            const int messageCount = 100;
+
+            for (var i = 0; i < messageCount; i++)
+            {
+                var state = new State { Value = i };
+                var requestTimeoutCommand = Timeout.BuildRequest(Guid.NewGuid().ToString(), DateTime.Now.AddSeconds(1), state, bus.PeerId.ToString());
+                bus.Send(requestTimeoutCommand);
+            }
+
+            Thread.Sleep(2.Seconds());
+
+            handler.Commands.Count.ShouldEqual(messageCount);
+
+            bus.Stop();
+        }
+
+        [Test]
+        public void SendTimeoutRequestAndWaitForTimeout()
+        {
+            var handler = new TimeoutCommandHandler();
+
+            var bus = CreateBusFactory()
+                .WithHandlers(typeof(TimeoutCommandHandler))
+                .ConfigureContainer(x => x.ForSingletonOf<TimeoutCommandHandler>().Use(handler))
+                .CreateAndStartBus();
+
+            bus.Subscribe(Subscription.Matching<TimeoutCommand>(x => x.ServiceName == bus.PeerId.ToString()));
+
+            var state = new State { Value = 42 };
+            var requestTimeoutCommand = Timeout.BuildRequest("Testing", DateTime.Now.AddSeconds(15), state, bus.PeerId.ToString());
+            bus.Send(requestTimeoutCommand);
+
+            var stopwatch = Stopwatch.StartNew();
+
+            if (handler.Signal.WaitOne(100.Seconds()))
+            {
+                Console.WriteLine("Yay for the orange game!");
+                Console.WriteLine("Elapsed: {0}", stopwatch.Elapsed);
+            }
+
+            bus.Stop();
+        }
+
+        [Test]
+        public void StartBusAndGetRidOfItLikeABadBoy()
+        {
+            CreateBusFactory().CreateAndStartBus();
+        }
+
+        [Test]
+        public void StartBusAndStopItLikeABoyScout()
+        {
+            var bus = CreateBusFactory().CreateAndStartBus();
+
+            Thread.Sleep(1000);
+
+            bus.Stop();
+        }
+
+        [Test]
+        public void PublishManualEvent()
+        {
+            using (var bus = CreateBusFactory().WithHandlers(typeof(ManualEventHandler)).CreateAndStartBus())
+            {
+                using (LocalDispatch.Disable())
+                {
+                    bus.Publish(new ManualEvent(42));
+                }
+
+                Wait.Until(() => ManualEventHandler.ReceivedEventCount == 1, 300.Seconds());
+            }
+        }
+
+        private static BusFactory CreateBusFactory()
+        {
+            return new BusFactory()
+                .WithConfiguration("tcp://localhost:129", "Dev");
+        }
+
+        [ProtoContract]
+        public class ManualEvent : IEvent
+        {
+            [ProtoMember(1, IsRequired = true)]
+            public readonly int Id;
+
+            public ManualEvent(int id)
+            {
+                Id = id;
+            }
+        }
+
         [ProtoContract]
         public class State
         {
@@ -47,75 +152,16 @@ namespace Abc.Zebus.Tests.Core
             }
         }
 
-        [Test]
-        public void SendSeveralTimeoutRequestAndCheckForResponseUnicity()
+        public class ManualEventHandler : IMessageHandler<ManualEvent>
         {
-            var handler = new AccumulatingTimeoutCommandHandler();
+            public static int ReceivedEventCount;
 
-            var bus = new BusFactory() 
-                .WithHandlers(typeof(AccumulatingTimeoutCommandHandler))
-                .ConfigureContainer(x => x.ForSingletonOf<AccumulatingTimeoutCommandHandler>().Use(handler))
-                .CreateAndStartBus();
-
-            bus.Subscribe(Subscription.Matching<TimeoutCommand>(x => x.ServiceName == bus.PeerId.ToString()));
-
-            const int messageCount = 100;
-
-            for (var i = 0; i < messageCount; i++)
+            public void Handle(ManualEvent message)
             {
-                var state = new State { Value = i };
-                var requestTimeoutCommand = Timeout.BuildRequest(Guid.NewGuid().ToString(), DateTime.Now.AddSeconds(1), state, bus.PeerId.ToString());
-                bus.Send(requestTimeoutCommand);
+                Interlocked.Increment(ref ReceivedEventCount);
+
+                Console.WriteLine(message.Id);
             }
-
-            Thread.Sleep(2.Seconds());
-
-            handler.Commands.Count.ShouldEqual(messageCount);
-
-            bus.Stop();
-        }
-
-        [Test]
-        public void SendTimeoutRequestAndWaitForTimeout()
-        {
-            var handler = new TimeoutCommandHandler();
-
-            var bus = new BusFactory()
-                .WithHandlers(typeof(TimeoutCommandHandler))
-                .ConfigureContainer(x => x.ForSingletonOf<TimeoutCommandHandler>().Use(handler))
-                .CreateAndStartBus();
-
-            bus.Subscribe(Subscription.Matching<TimeoutCommand>(x => x.ServiceName == bus.PeerId.ToString()));
-
-            var state = new State { Value = 42 };
-            var requestTimeoutCommand = Timeout.BuildRequest("Testing", DateTime.Now.AddSeconds(15), state, bus.PeerId.ToString());
-            bus.Send(requestTimeoutCommand);
-
-            var stopwatch = Stopwatch.StartNew();
-
-            if (handler.Signal.WaitOne(100.Seconds()))
-            {
-                Console.WriteLine("Yay for the orange game!");
-                Console.WriteLine("Elapsed: {0}", stopwatch.Elapsed);
-            }
-
-            bus.Stop();
-        }
-
-        [Test]
-        public void StartBusAndGetRidOfItLikeABadBoy()
-        {
-            new BusFactory().CreateAndStartBus();
-        }
-
-        [Test]
-        public void StartBusAndStopItLikeABoyScout()
-        {
-            var bus = new BusFactory().CreateAndStartBus();
-
-            Thread.Sleep(1000);
-
-            bus.Stop();
         }
     }
 }
