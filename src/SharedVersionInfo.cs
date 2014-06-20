bc23aff33deab2fd436fe77cccee3886db612552
Kevin Lovato <alprema@alprema.com>
2014-06-20 16:18:00 +0200

    Added missing reference to the Directory.Cassandra nuspec + Switched to 1.0.10

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.9")]
-[assembly: AssemblyFileVersion("1.0.9")]
-[assembly: AssemblyInformationalVersion("1.0.9")]
+[assembly: AssemblyVersion("1.0.10")]
+[assembly: AssemblyFileVersion("1.0.10")]
+[assembly: AssemblyInformationalVersion("1.0.10")]
