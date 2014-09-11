7311d6a9312dc2fc4166474caad9377ff3482b47
Kevin Lovato <alprema@alprema.com>
2014-09-11 19:12:49 +0200

    Switched to version 1.1.6-cassandra-updt

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.5")]
-[assembly: AssemblyFileVersion("1.1.5")]
-[assembly: AssemblyInformationalVersion("1.1.5")]
+[assembly: AssemblyVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyInformationalVersion("1.1.6-cassandra-updt")]
