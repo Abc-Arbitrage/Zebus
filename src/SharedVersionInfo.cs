2ffded0a064e209e38aa96e4ba972548194d0a80
Kevin Lovato <alprema@alprema.com>
2014-09-26 13:38:04 +0200

    Switched to 1.1.8

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.7")]
-[assembly: AssemblyFileVersion("1.1.7")]
-[assembly: AssemblyInformationalVersion("1.1.7")]
+[assembly: AssemblyVersion("1.1.8")]
+[assembly: AssemblyFileVersion("1.1.8")]
+[assembly: AssemblyInformationalVersion("1.1.8")]
