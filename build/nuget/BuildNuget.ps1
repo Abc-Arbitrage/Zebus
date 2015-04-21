7b10dd277d24bdbdff8dc42bca875cf396832c75
Kevin Lovato <alprema@alprema.com>
2015-04-21 16:24:18 +0200

    Add nuget restore in release script

--- build/nuget/BuildNuget.ps1
+++ build/nuget/BuildNuget.ps1
@@ -10,6 +10,7 @@ if((Test-Path $outputLocation) -eq $true)
 $dir = New-Item -ItemType directory $outputLocation;
 
 # Compile solution in release
+& '.\tools\nuget\NuGet.exe' restore .\src\Abc.Zebus.sln
 & $msbuild .\src\Abc.Zebus.sln /t:rebuild /p:Configuration=Release
 
 # Get metadata (without locking file)
