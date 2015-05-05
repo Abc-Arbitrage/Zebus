6cc486a2e935ba8beabda7c528eb7b9369d00859
Kevin Lovato <alprema@alprema.com>
2015-05-05 15:05:10 +0200

    Switch to version 1.2.2-new-subs01

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.13")]
-[assembly: AssemblyFileVersion("1.2.13")]
-[assembly: AssemblyInformationalVersion("1.2.13")]
+[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.2")]
+[assembly: AssemblyInformationalVersion("1.2.2-new-subs01")]
