392a527f982dd1fdb43071a1df622b214f434104
mendelmonteiro <m.monteiro@abc-arbitrage.com>
2015-07-06 18:05:58 +0200

    Switch to version 1.3.4

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.3")]
-[assembly: AssemblyFileVersion("1.3.3")]
-[assembly: AssemblyInformationalVersion("1.3.3")]
+[assembly: AssemblyVersion("1.3.4")]
+[assembly: AssemblyFileVersion("1.3.4")]
+[assembly: AssemblyInformationalVersion("1.3.4")]
