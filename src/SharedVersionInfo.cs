df89f7eab590befe8354227fa31895beb90a7254
Kevin Lovato <alprema@alprema.com>
2015-07-29 13:26:00 +0200

    Switch to version 1.3.6

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.5")]
-[assembly: AssemblyFileVersion("1.3.5")]
-[assembly: AssemblyInformationalVersion("1.3.5")]
+[assembly: AssemblyVersion("1.3.6")]
+[assembly: AssemblyFileVersion("1.3.6")]
+[assembly: AssemblyInformationalVersion("1.3.6")]
