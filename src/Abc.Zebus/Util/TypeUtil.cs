0d0d05e52c2d84ec838b0670c4b26329fcaee96a
Antoine Blanchet <a.blanchet@abc-arbitrage.com>
2015-08-11 15:32:13 +0200

    Remove legacy code (Ensure Abc assemblies are loaded)

--- src/Abc.Zebus/Util/TypeUtil.cs
+++ src/Abc.Zebus/Util/TypeUtil.cs
@@ -3,26 +3,18 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using log4net;
 
 namespace Abc.Zebus.Util
 {
     internal static class TypeUtil
     {
-        private static readonly ILog _logger = LogManager.GetLogger(typeof(TypeUtil));
         private static readonly ConcurrentDictionary<string, Type> _typesByNames = new ConcurrentDictionary<string, Type>();
 
         public static Type Resolve(string typeName)
         {
             return _typesByNames.GetOrAdd(typeName, FindTypeByName);
         }
-
-        public static void EnsureAbcAssembliesAreLoaded()
-        {
-            AssemblyLoader.EnsureAbcAssembliesAreLoaded();
-        }
-
+        
         private static Type FindTypeByName(string typeName)
         {
             try
@@ -35,12 +27,9 @@ namespace Abc.Zebus.Util
             {
             }
 
-            AssemblyLoader.EnsureAbcAssembliesAreLoaded();
-
             if (typeName.Contains("<"))
                 return FindGenericTypeByName(typeName);
             
-            // OrderBy(a => a.FullName) because 99% of requested types will be in Abc assemblies
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().OrderBy(a => a.FullName))
             {
                 var type = assembly.GetType(typeName);
@@ -53,7 +42,7 @@ namespace Abc.Zebus.Util
 
         private static Type FindGenericTypeByName(string typeName)
         {
-            var genericArguments = typeName.Substring(typeName.IndexOf("<", System.StringComparison.Ordinal)).Trim('<', '>').Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var genericArguments = typeName.Substring(typeName.IndexOf("<", StringComparison.Ordinal)).Trim('<', '>').Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
             var typeNameWithoutGenericArguments = typeName.Substring(0, typeName.IndexOf("<", StringComparison.Ordinal)) + '`' + genericArguments.Length;
 
 
@@ -70,35 +59,6 @@ namespace Abc.Zebus.Util
                 genericTypes.Add(genericType);
             }
             return type.MakeGenericType(genericTypes.ToArray());
-        }
-
-        private static class AssemblyLoader
-        {
-            static AssemblyLoader()
-            {
-                LoadAssemblies("dll");
-                LoadAssemblies("exe");
-            }
-
-            private static void LoadAssemblies(string extension)
-            {
-                foreach (var assemblyPath in System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "Abc.*." + extension))
-                {
-                    var assemblyName = Path.GetFileNameWithoutExtension(assemblyPath);
-                    try
-                    {
-                        Assembly.Load(assemblyName);
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.WarnFormat("Unable to load assembly {0}: {1}", assemblyName, ex);
-                    }
-                }
-            }
-
-            public static void EnsureAbcAssembliesAreLoaded()
-            {
-            }
         }
     }
 }
