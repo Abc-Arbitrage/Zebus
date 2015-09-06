39047013d12c0dc386f8bd819639d4981f6e1475
Antoine Blanchet <antoine.blanchet@gmail.com>
2015-09-06 21:42:25 +0200

    Improve UpdatePeerSubscriptionsCommand ToString()

--- src/Abc.Zebus/Directory/UpdatePeerSubscriptionsCommand.cs
+++ src/Abc.Zebus/Directory/UpdatePeerSubscriptionsCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using ProtoBuf;
 
 namespace Abc.Zebus.Directory
@@ -24,7 +25,7 @@ namespace Abc.Zebus.Directory
 
         public override string ToString()
         {
-            return string.Format("{0} TimestampUtc: {1:yyyy-MM-dd HH:mm:ss.fff}", PeerId, TimestampUtc);
+            return string.Format("PeerId: {0}, TimestampUtc: {1:yyyy-MM-dd HH:mm:ss.fff}, Subscriptions: [{2}]", PeerId, TimestampUtc, string.Join(", ", Subscriptions.AsEnumerable()));
         }
     }
 }
