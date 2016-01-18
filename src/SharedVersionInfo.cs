4fcecd93b3dd439be3270578e3e17ab0cde644a8
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2016-01-18 11:17:12 +0100

    Switch to version 1.4.1

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
-[assembly: AssemblyInformationalVersion("1.4.0")]
+[assembly: AssemblyVersion("1.4.1")]
+[assembly: AssemblyFileVersion("1.4.1")]
+[assembly: AssemblyInformationalVersion("1.4.1")]
