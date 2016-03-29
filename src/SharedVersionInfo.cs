b7b54d1c9a840773c3cccdbac167892cec10914a
mendelmonteiro <m.monteiro@abc-arbitrage.com>
2016-03-29 15:49:21 +0200

    Switch to version 1.4.6

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("1.4.6")]
 [assembly: AssemblyFileVersion("1.4.6")]
-[assembly: AssemblyInformationalVersion("1.4.6-subsfix01")]
+[assembly: AssemblyInformationalVersion("1.4.6")]
