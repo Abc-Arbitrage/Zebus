2bdc7123a767ee5e057d5e78a041e59dc96c9df5
Kevin Lovato <alprema@alprema.com>
2016-04-25 11:00:54 +0200

    Switch to version 1.2.10

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.9")]
-[assembly: AssemblyFileVersion("1.2.9")]
-[assembly: AssemblyInformationalVersion("1.2.9")]
+[assembly: AssemblyVersion("1.2.10")]
+[assembly: AssemblyFileVersion("1.2.10")]
+[assembly: AssemblyInformationalVersion("1.2.10")]
