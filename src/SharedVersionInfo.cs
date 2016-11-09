0869d291a08685c94f824ed13f33ef69b4f4711b
Bertrand Blackburn <b.blackburn@abc-arbitrage.com>
2016-11-09 18:21:42 +0100

    Fix SharedVersionInfo.cs version

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.1-beta")]
-[assembly: AssemblyFileVersion("1.5.1-beta")]
+[assembly: AssemblyVersion("1.5.1")]
+[assembly: AssemblyFileVersion("1.5.1")]
 [assembly: AssemblyInformationalVersion("1.5.1-beta")]
