d81e662364763b4014782cbd04fb53600c5cc938
Bertrand Blackburn <b.blackburn@abc-arbitrage.com>
2016-11-09 18:14:48 +0100

    Switch to version 1.5.1-beta

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyVersion("1.5.1-beta")]
+[assembly: AssemblyFileVersion("1.5.1-beta")]
+[assembly: AssemblyInformationalVersion("1.5.1-beta")]
