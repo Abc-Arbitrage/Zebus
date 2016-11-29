9ddb35f35e7f5a347e1f02b2c6da79cf627f80b9
Antoine Blanchet <a.blanchet@abc-arbitrage.com>
2016-11-29 17:55:21 +0100

    Update to Zebus 1.5.1-beta2 and switch to 1.3.2-beta

--- src/Abc.Zebus.Directory/Configuration/AppSettingsBusConfiguration.cs
+++ src/Abc.Zebus.Directory/Configuration/AppSettingsBusConfiguration.cs
@@ -10,6 +10,7 @@ namespace Abc.Zebus.Directory.Configuration
             RegistrationTimeout = AppSettings.Get("Bus.Directory.RegistrationTimeout", 30.Seconds());
             StartReplayTimeout = AppSettings.Get("Bus.Persistence.StartReplayTimeout", 30.Seconds());
             IsDirectoryPickedRandomly = AppSettings.Get("Bus.Directory.PickRandom", true);
+            IsErrorPublicationEnabled = AppSettings.Get("Bus.IsErrorPublicationEnabled", true);
         }
 
         public string[] DirectoryServiceEndPoints { get { return new string[0]; } }
@@ -18,5 +19,6 @@ namespace Abc.Zebus.Directory.Configuration
         public TimeSpan RegistrationTimeout { get; private set; }
         public TimeSpan StartReplayTimeout { get; private set; }
         public bool IsDirectoryPickedRandomly { get; private set; }
+        public bool IsErrorPublicationEnabled { get; private set; }
     }
 }
