b304b03f34ecf1e9649607b598adefe4a99257cf
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2017-02-02 11:20:50 +0100

    Add MessageHandlerInvokerLoaderTests

---
+++ src/Abc.Zebus.Tests/Scan/MessageHandlerInvokerLoaderTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Abc.Zebus.Core;
+using Abc.Zebus.Testing;
+using Abc.Zebus.Testing.Extensions;
+using Abc.Zebus.Util;
+using NUnit.Framework;
+
+namespace Abc.Zebus.Tests.Scan
+{
+    [TestFixture]
+    public class MessageHandlerInvokerLoaderTests
+    {
+        [Test]
+        public void should_create_all_invoker_types()
+        {
+            var bus = new BusFactory()
+                .WithHandlers(
+                    typeof(SyncMessageHandler),
+                    typeof(AsyncMessageHandler),
+                    typeof(BatchedMessageHandler)
+                )
+                .CreateAndStartInMemoryBus();
+
+            var message = new TestMessage();
+            bus.Publish(message);
+            bus.Stop();
+
+            message.HandledSync.ShouldBeTrue();
+            message.HandledAsync.ShouldBeTrue();
+            message.HandledBatched.ShouldBeTrue();
+        }
+
+        public class TestMessage : IEvent
+        {
+            public bool HandledSync { get; set; }
+            public bool HandledAsync { get; set; }
+            public bool HandledBatched { get; set; }
+        }
+
+        public class SyncMessageHandler : IMessageHandler<TestMessage>
+        {
+            public void Handle(TestMessage message)
+            {
+                message.HandledSync = true;
+            }
+        }
+
+        public class AsyncMessageHandler : IAsyncMessageHandler<TestMessage>
+        {
+            public Task Handle(TestMessage message)
+            {
+                message.HandledAsync = true;
+                return TaskUtil.Completed;
+            }
+        }
+
+        public class BatchedMessageHandler : IBatchedMessageHandler<TestMessage>
+        {
+            public void Handle(IList<TestMessage> messages)
+            {
+                foreach (var message in messages)
+                    message.HandledBatched = true;
+            }
+        }
+    }
+}
