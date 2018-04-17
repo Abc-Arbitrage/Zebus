d33782776ac65ef086ffba948943255aab89c0e3
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-04-17 10:12:27 +0200

    Update to Zebus 2.2.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyDescription("The Directory service used by Zebus - https://github.com/Abc-Arbitrage/Zebus.Directory")]
 [assembly: AssemblyCompany("ABC arbitrage")]
 [assembly: AssemblyProduct("Zebus.Directory")]
-[assembly: AssemblyCopyright("Copyright © ABC arbitrage 2017")]
+[assembly: AssemblyCopyright("Copyright © ABC arbitrage 2018")]
