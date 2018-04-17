ef1e963580f3c1debaa340cfc15f6fbf73cfce3f
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-04-17 10:30:51 +0200

    Update to Zebus 2.2.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -3,5 +3,4 @@
 [assembly: AssemblyDescription("The Persistence service used by Zebus - https://github.com/Abc-Arbitrage/Zebus.Persistence")]
 [assembly: AssemblyCompany("ABC arbitrage")]
 [assembly: AssemblyProduct("Zebus.Persistence")]
-[assembly: AssemblyCopyright("Copyright © ABC arbitrage 2017")]
-
+[assembly: AssemblyCopyright("Copyright © ABC arbitrage 2018")]
