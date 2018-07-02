1b33a5bd9d24fe3380ed806abd809b48f4a1da4b
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-07-02 18:54:11 +0200

    Update build script

--- src/Abc.Zebus.Testing/Properties/AssemblyInfo.cs
+++ src/Abc.Zebus.Testing/Properties/AssemblyInfo.cs
@@ -1,11 +1,9 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Abc.Zebus.Testing")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2b782073-d62a-4e5e-91ed-e5c2290974b3")]
 
