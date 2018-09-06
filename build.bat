85173d53e81d965e86310fbafe900f14db3ff7bc
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-09-06 21:59:07 +0200

    Use embedded debug info for maximum SourceLink compatibility

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
+@echo off
+rem We're not really on AppVeyor but this disables the Cassandra node requirement
+set APPVEYOR=True
 powershell -ExecutionPolicy Bypass build\build.ps1 -Script build\build.cake -Target Build
 pause
