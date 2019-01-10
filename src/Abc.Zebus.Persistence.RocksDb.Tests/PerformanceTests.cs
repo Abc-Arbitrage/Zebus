aa037dc1c3c6539de9bdffd2668cdac32ccefb26
Mendel Monteiro-Beckerman <mendel.monteiro@gmail.com>
2019-01-10 16:28:22 +0100

    Persistence: [RocksDb] add replay performance test

--- src/Abc.Zebus.Persistence.RocksDb.Tests/PerformanceTests.cs
+++ src/Abc.Zebus.Persistence.RocksDb.Tests/PerformanceTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,13 +22,13 @@ namespace Abc.Zebus.Persistence.RocksDb.Tests
         public void SetUp()
         {
             _storage = new RocksDbStorage(Guid.NewGuid().ToString());
-            _storage.Start(); 
+            _storage.Start();
         }
 
         [TearDown]
         public void Teardown()
         {
-            _storage.Stop(); 
+            _storage.Stop();
         }
 
         [Test]
@@ -75,16 +76,48 @@ namespace Abc.Zebus.Persistence.RocksDb.Tests
             await Task.WhenAll(writeTask/*, updatesTask*/);
         }
 
+        [Test]
+        public void should_test_replay()
+        {
+            var messageBytes = MessageBytes();
+
+            // Fill with lots of unacked messages
+            var entriesToPersist = new List<MatcherEntry>();
+            var peerId = new PeerId("Peer");
+            for (int i = 0; i < 10_000; i++)
+            {
+                MessageId.PauseIdGenerationAtDate(SystemDateTime.UtcNow.Date.AddSeconds(i * 10));
+                entriesToPersist.Add(MatcherEntry.Message(peerId, MessageId.NextId(), new MessageTypeId("SomeEvent"), messageBytes));
+            }
+
+            _storage.Write(entriesToPersist);
+
+            // Read all unacked messages 
+            var messageReader = _storage.CreateMessageReader(peerId);
+            var startTime = DateTime.UtcNow;
+            var testDuration = 30.Seconds();
+            var count = 0;
+            while (DateTime.UtcNow - startTime < testDuration)
+            {
+                foreach (var transportMessage in messageReader.GetUnackedMessages()) { }
+
+                count++;
+            }
+
+            Console.WriteLine($"Replayed {count:N0} times ({count*entriesToPersist.Count:N0} messages) in {testDuration.TotalSeconds:N0}s");
+        }
+
         private List<MatcherEntry> ToAckEntries(List<MatcherEntry> entriesToPersist)
         {
             return entriesToPersist.Select(x => MatcherEntry.Ack(x.PeerId, x.MessageId)).ToList();
         }
 
-        private static List<MatcherEntry> GetEntriesToPersist(byte[] messageBytes, int offset)
+        private static List<MatcherEntry> GetEntriesToPersist(byte[] messageBytes, int offset, int count = 100)
         {
             var entriesToPersist = new List<MatcherEntry>();
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < count; i++)
             {
+                MessageId.PauseIdGenerationAtDate(SystemDateTime.UtcNow.Date.AddSeconds(i * 10));
                 entriesToPersist.Add(MatcherEntry.Message(new PeerId("Peer" + (i + offset)), MessageId.NextId(), new MessageTypeId("SomeEvent"), messageBytes));
             }
             return entriesToPersist;
