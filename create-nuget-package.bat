f333ffe6f4d1db6cab08da148a28b9252566ba5b
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2019-01-21 14:37:44 +0100

    Update and cleanup Cake

--- create-nuget-package.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem We're not really on AppVeyor but this disables the Cassandra node requirement
-set APPVEYOR=True
-powershell -ExecutionPolicy Bypass build\build.ps1 -Script build\build.cake -Target Nuget
-pause
