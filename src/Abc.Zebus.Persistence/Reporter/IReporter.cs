34957ef3c639f3e7a7df0f809781eca1ef34c25a
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2022-09-27 07:48:15 +0800

    Delete TakeAndReset methods on IReporter

--- src/Abc.Zebus.Persistence/Reporter/IReporter.cs
+++ src/Abc.Zebus.Persistence/Reporter/IReporter.cs
@@ -1,13 +1,8 @@
-using System.Collections.Generic;
-
 namespace Abc.Zebus.Persistence.Reporter
 {
     public interface IReporter
     {
         void AddReplaySpeedReport(ReplaySpeedReport replaySpeedReport);
-        IList<ReplaySpeedReport> TakeAndResetReplaySpeedReports();
-
         void AddStorageReport(StorageReport storageReport);
-        IList<StorageReport> TakeAndResetStorageReports();
     }
 }
