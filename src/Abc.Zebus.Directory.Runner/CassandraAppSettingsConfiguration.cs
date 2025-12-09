ae6038959db281ce857696debe8f2aff34eca6ab
Reda Bouallou <r.bouallou@abc-arbitrage.com>
2025-12-09 12:33:40 +0100

    Directory.Runner: Fix build

--- src/Abc.Zebus.Directory.Runner/CassandraAppSettingsConfiguration.cs
+++ src/Abc.Zebus.Directory.Runner/CassandraAppSettingsConfiguration.cs
@@ -12,11 +12,13 @@ namespace Abc.Zebus.Directory.Runner
             KeySpace = AppSettings.Get("Cassandra.KeySpace", "");
             QueryTimeout = AppSettings.Get("Cassandra.QueryTimeout", 5.Seconds());
             LocalDataCenter = AppSettings.Get("Cassandra.LocalDataCenter", "");
+            UseSsl = AppSettings.Get("Cassandra.UseSsl", false);
         }
 
         public string Hosts { get; }
         public string KeySpace { get; }
         public TimeSpan QueryTimeout { get; }
         public string LocalDataCenter { get; }
+        public bool UseSsl { get; }
     }
 }
